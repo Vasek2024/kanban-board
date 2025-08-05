19d9795c83244901f9693a75f27af8a1a120838f
Vasek2024 <p.f1@yandex.ru>
2025-08-05 23:11:10 +0700

    css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
+import './Footer.css'
+
 function Footer() {
 
     return (
       <div className='footer'>
+        <ul className='footer__finished'>
+            <li>Active tasks: &lt;N&gt;</li>
+            <li>Finished tasks: &lt;M&gt;</li>
+        </ul>
+        <ul className='footer__kanban'>
+            <li>Kanban board by &lt;NAME&gt;, &lt;YEAR&gt;</li>
+        </ul>
           {/* <div className='header__title'>Awesome Kanban Board</div>
           <div className='header__burger'>
               <div className='burger__icon'>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,47 @@
+import './Main.css'
 function Main() {
 
     return (
       <div className='main'>
-          {/* <div className='header__title'>Awesome Kanban Board</div>
-          <div className='header__burger'>
-              <div className='burger__icon'>
-                  <image className='burger__icon-avatar'></image>
-                  <image className='burger__icon-closed'></image>
-                  <image className='burger__icon-open'></image>
-              </div>
-              <div className='burger__registration'>
-                  <ul className='burger__registration-profile'>
-                      <li className='burger__registration-profile'>Profile</li>
-                      <li className='burger__registration-log'>Log Out</li>
-                  </ul>
-              </div>
-          </div> */}
+            <div className='main__backlog'>
+                <h2 className='main__backlog-h2'>Backlog</h2>
+                <h3 className='main__backlog-h3'>Login page – performance issues</h3>
+                <ul className='main__backlog-list'>
+                    <li>Sprint bugfix</li>
+                </ul>
+            </div>
+
+            <div className='main__ready'>
+                <h2 className='main__ready-h2'>Ready</h2>
+                <h3 className='main__ready-h3'>Shop page – performance issues</h3>
+                <ul className='main__ready-list'>
+                    <li>Checkout bugfix</li>
+                    <li>Shop bug1</li>
+                    <li>Shop bug2</li>
+                    <li>Shop bug3</li>
+                    <li>Shop bug4</li>
+                    <li>Shop bug5</li>
+                    <li>Shop bug6</li>
+                    <li>Shop bug7</li>
+                    <li>Shop page – performance issues</li>
+                </ul>
+            </div>
+
+            <div className='main__progress'>
+                <h2 className='main__progress-h2'>In Progress</h2>
+                <h3 className='main__progress-h3'>User page – performance issues</h3>
+                <ul className='main__progress-list'>
+                    <li>Auth bugfix</li>
+                </ul>
+            </div>
+
+            <div className='main__finished'>
+                <h2 className='main__finished-h2'>Finished</h2>
+                <h3 className='main__finished-h3'>Main page – performance issues</h3>
+                <ul className='main__finished-list'>
+                    <li>Main page bugfix</li>
+                </ul>
+            </div>
       </div>
     )
   }
